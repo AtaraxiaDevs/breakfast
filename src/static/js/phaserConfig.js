41bf4ab93c440e8e0e05e32496002c44ff6f60e5
FRoZeNDanieh <d.ayllon.999@gmail.com>
2020-10-14 12:23:55 +0200

    Update phaserConfig.js

--- src/static/js/phaserConfig.js
+++ src/static/js/phaserConfig.js
@@ -1,7 +1,13 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: 'phaser-example',
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600
+  },
+  backgroundColor: 0x444444,
   scene: {
     preload: preload,
     create: create,
@@ -11,14 +17,35 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {}
-function create() {}
+function preload() {
+  this.load.image("s_provisional", "resources/s_provisional.png")
+}
+
+function create() {
+  this.add.image(200, 400, 's_provisional')
+}
+
 function update() {}
 
-var IS_TOUCH = false;
+function resize(){​​
+  var canvas = document.querySelector("canvas");
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  var windowRatio = windowWidth / windowHeight;
+  var gameRatio = game.config.width / game.config.height;
+
+  if(windowRatio < gameRatio){​​
+      canvas.style.width = windowWidth + "px";
+      canvas.style.height = (windowWidth / gameRatio) + "px";
+  }​​
+  else {​​
+      canvas.style.width = (windowHeight * gameRatio) + "px";
+      canvas.style.height = windowHeight + "px";
+  }​​
+}​​
+
 window.addEventListener("touchstart", function () {
-  IS_TOUCH = true;
+  resize()
 });
 
-console.log(IS_TOUCH);
 alert("SOY UN PUTO MOVIL GRACIAS");
