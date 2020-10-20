b582fca60d63e046c7d8c70f0f051697164bc4b1
albertosanchezmateo <35465627+albertosanchezmateo@users.noreply.github.com>
2020-10-20 17:24:25 +0200

    Ajustes de base de datos y corrección de errores

--- src/static/js/bdPersonajes.js
+++ src/static/js/bdPersonajes.js
@@ -1,11 +1,11 @@
 let bd = {
-    "placeHolder":{
+    "veloz":{
         "atk":  4,
         "hp":   9,
         "vel":  15,
         "dps":  1,
         "rango": 1,
-        "sprite": "s_provisional"
+        "sprite": "sp_velozIddle"
     },
     "placeHolder2":{
         "atk":  4,
