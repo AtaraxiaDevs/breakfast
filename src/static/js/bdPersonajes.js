2d6afe8c62bf149e4b19bea043d94128eefb21af
MeSientoYMeCallo99 <72436411+MeSientoYMeCallo99@users.noreply.github.com>
2020-11-19 12:45:19 +0100

    nivelando

--- src/static/js/bdPersonajes.js
+++ src/static/js/bdPersonajes.js
@@ -1,13 +1,13 @@
 let bdInfoPersonajes = {
     "veloz":{
-        "atk":  4,
+        "atk":  6,
         "hp":   3,
         "vel":  15,
-        "dps":  1,
+        "dps":  0.5,
         "rango": 1,
     },
     "tank":{
-        "atk":  2,
+        "atk":  2.5,
         "hp":   15,
         "vel":  5,
         "dps":  2,
@@ -15,7 +15,7 @@ let bdInfoPersonajes = {
     },
 
     "distancia":{
-        "atk":  2.5,
+        "atk":  3.5,
         "hp":   7,
         "vel":  10,
         "dps":  1.5,
